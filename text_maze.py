4faddb2d82db7c9ac8bdb86ba5aec2cda97750aa
Eduardo Bautista Barrera <eg.bb952@gmail.com>
2020-09-29 11:05:20 -0500

    added a new stage

--- text_maze.py
+++ text_maze.py
@@ -6,7 +6,17 @@ def run():
         print(f"Okay {name}, you are old enough to play, lets continue")
         place = input("Which place do you like? (woods/beach)")
         
-        if 
+        if
+
+    elif age == 25:
+        print("You are an special player, so let's start an special game")
+        like = input("Which of these food do you like most? (lasagna/pizza) ")
+        
+        if like == "lasagna":
+            pass
+        else:
+            pass
+
     else:
         print("Sorry you are too young to play")
 
