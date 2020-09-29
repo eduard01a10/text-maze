ea3a5565ba066a0149c463a66b2fe1472dabdabf
Eduardo Bautista Barrera <eg.bb952@gmail.com>
2020-09-28 19:24:06 -0500

    First if's in dev branch

--- text_maze.py
+++ text_maze.py
@@ -1,5 +1,15 @@
 def run():
     name = input("What is your name? ")
     age = int(input("what is your age? "))
+
+    if age >= 18:
+        print(f"Okay {name}, you are old enough to play, lets continue")
+        place = input("Which place do you like? (woods/beach)")
+        
+        if 
+    else:
+        print("Sorry you are too young to play")
+
+
 if __name__ == "__main__":
     run()
