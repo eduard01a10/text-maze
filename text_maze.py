936c55417d249afecf721e426327527d25cc4e08
Eduardo Bautista Barrera <eg.bb952@gmail.com>
2020-10-09 13:29:22 -0500

    Added the beach part

--- text_maze.py
+++ text_maze.py
@@ -1,12 +1,13 @@
-def chance():
+def chance():  ## This function is for the woods part
     chances = 4
+    
 
     while chances >= 0:
         answer = input("What is your answer? ")
 
         if answer == "2":
             print(f"Wrong answer, you have {chances} chances left")
-        elif answer == "triangle":
+        elif answer == "triangle" or answer == "paris".lower():
             print("You win!")
             break
         else:
@@ -23,11 +24,31 @@ def run():
         print(f"Okay {name}, you are old enough to play, lets continue")
         place = input("Which place do you like? (woods/beach) ")
         
+      ## Here it begins the part of the woods  
         if place == "woods":
             print("I really like this place. Now, solve this problem 1 + 1")
             print("You just have 5 chances")
             
             chance()
+
+      ## Here it begins the part of the beach  
+        elif place == "beach":
+            print("I don't really like this place, but anyway, pick a number")
+            
+            for i in range(1, 11):
+                print(i)
+                
+            choice = int(input("Which one do you like? "))
+
+            if choice >= 0 and choice <= 5:
+                print("You have to answer a little question. What is the capital of France?")
+
+                chance()
+
+            elif choice >= 5 and choice <= 10:
+                pass 
+
+                
 
     elif age == 25:
         print("You are an special player, so let's start an special game")
